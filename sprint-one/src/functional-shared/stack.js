3cfe498e36b90c4d04d4531b92d5de4811ffd792
FrankBowers24 <FrankBowers24@gmail.com>
2015-03-25 14:32:06 -0700

    First Commit

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.size = 0;
+  obj.push = stackMethods.push;
+  obj.pop = stackMethods.pop;
+  obj.size = stackMethods.size;
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push:function(value){
+    this.storage[this.size++] = value;
+  },
+
+  pop: function(){
+    if (this.size > 0) {
+      return this.storage[--this.size];
+    }
+  },
+
+  size: function(){
+    return this.size;
+  }
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
-var Queue = function(){
+var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value){
+  someInstance.enqueue = function(value) {
+    storage[size++] = value;
   };
 
-  someInstance.dequeue = function(){
+  someInstance.dequeue = function() {
+    if (size > 0) {
+      var result = storage[0];
+      size--;
+      for (var i = 0; i < size; i++) {
+        storage[i] = storage[i + 1];
+      }
+      return result;
+    }
   };
 
-  someInstance.size = function(){
+  someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
