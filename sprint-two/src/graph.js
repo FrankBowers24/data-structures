a0a8e903c95c30c87b2830d4b1ffb1efeab5279a
FrankBowers24 <FrankBowers24@gmail.com>
2015-03-26 16:22:22 -0700

    finished to set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -5,38 +5,77 @@ var Graph = function(){
   this.nextIndex = 0;
 };
 
-var GraphNode = function(value)
+var GraphNode = function(value, index)
 {
+  this.graphIndex = index;
   this.value = value;
   this.edges = {};
-  this.nextEdgeIndex = 0;
-}
+};
 
 Graph.prototype.addNode = function(nodeValue){
-  this.storage[this.nextIndex++] = new GraphNode(nodeValue);
+  this.storage[this.nextIndex] = new GraphNode(nodeValue, this.nextIndex);
+  this.nextIndex++;
 };
 
 Graph.prototype.contains = function(nodeValue){
-  var result = false;
+  return this.findNodeIndex(nodeValue) !== null;
+};
+
+Graph.prototype.findNodeIndex = function(nodeValue){
+  var graphIndex = null;
   _.each(this.storage, function(node){
-    if(!result){ result = node.value === nodeValue; }
+    if(!graphIndex){
+      if(node.value === nodeValue){
+        graphIndex = node.graphIndex;
+      }
+    }
   });
-  return result;
+  return graphIndex;
 };
 
 Graph.prototype.removeNode = function(nodeValue){
+  var node = this.storage[this.findNodeIndex(nodeValue)];
+  if(node !== null){
+    _.each(node.edges, function(edgeNode){
+      delete(edgeNode.edges[node.graphIndex]);
+    });
+    delete(this.storage[node.graphIndex]);
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNodeValue, toNodeValue){
+  var fromIndex = this.findNodeIndex(fromNodeValue);
+  var result = false;
+  if(fromIndex !== null){
+    _.each(this.storage[fromIndex].edges, function(nodeValue){
+      if(!result){ result = nodeValue === toNodeValue; }
+    });
+  }
+  return result;
 };
 
 Graph.prototype.addEdge = function(fromNodeValue, toNodeValue){
+  var fromNode = this.storage[this.findNodeIndex(fromNodeValue)];
+  var toNode = this.storage[this.findNodeIndex(toNodeValue)];
+  if(fromNode !== undefined && toNode !== undefined){
+    fromNode.edges[toNode.graphIndex] = toNode.value;
+    toNode.edges[fromNode.graphIndex] = fromNode.value;
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNodeValue, toNodeValue){
+  var fromNode = this.storage[this.findNodeIndex(fromNodeValue)];
+  var toNode = this.storage[this.findNodeIndex(toNodeValue)];
+  if(fromNode !== undefined && toNode !== undefined){
+    delete(fromNode.edges[toNode.graphIndex]);
+    delete(toNode.edges[fromNode.graphIndex]);
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.storage, function(node){
+    cb(node.value);
+  });
 };
 
 /*
