0de3d464bd2c15b0ced14892a7ae0086c77c3344
FrankBowers24 <FrankBowers24@gmail.com>
2015-03-27 12:05:36 -0700

    Finished required tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,54 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  if(this.value === value){
+    return;
+  } else if(this.value > value){
+    if(this.left === null){
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if(this.right === null){
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  var result = false;
+  if (this.value === value) {
+    result = true;
+  } else if (this.value > value) {
+    if (this.left !== null) {
+      result = this.left.contains(value);
+    }
+  } else {
+    if (this.right !== null) {
+      result = this.right.contains(value);
+    }
+  }
+  return result;
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,41 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucketList = this._storage.get(i);
+  if(bucketList === undefined || bucketList === null){
+    this._storage.set(i, [[k, v]]);
+  } else {
+    var keyFound = _.filter(bucketList, function(tuple){
+      var result = tuple[0] === k;
+      if(result){
+        tuple[1] = v;
+      }
+      return result;
+    }).length === 1;
+    if(!keyFound){
+      this._storage.set(bucketList.push([k, v]));
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucketList = this._storage.get(i);
+  if(bucketList === undefined || bucketList === null){
+    return null;
+  } else {
+    var result = null;
+    _.each(bucketList, function(tuple){
+      if(tuple[0] === k){
+        result = tuple[1];
+      }
+    });
+    return result;
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  this.insert(k, null);
 };
 
 
