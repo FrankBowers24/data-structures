03af430428729655fd76cb51ac988d6211285e9b
FrankBowers24 <FrankBowers24@gmail.com>
2015-03-27 19:59:46 -0700

    Partial implementation of hashtable extra credit

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,6 @@
 var HashTable = function(){
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -8,6 +9,8 @@ HashTable.prototype.insert = function(k, v){
   var bucketList = this._storage.get(i);
   if(bucketList === undefined || bucketList === null){
     this._storage.set(i, [[k, v]]);
+    this._count++;
+    this.resizeCheck();
   } else {
     var keyFound = _.filter(bucketList, function(tuple){
       var result = tuple[0] === k;
@@ -18,6 +21,8 @@ HashTable.prototype.insert = function(k, v){
     }).length === 1;
     if(!keyFound){
       this._storage.set(bucketList.push([k, v]));
+      this._count++;
+      this.resizeCheck();
     }
   }
 };
@@ -39,8 +44,47 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
-  this.insert(k, null);
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucketList = this._storage.get(i);
+  var index = null;
+  _.each(bucketList, function(tuple, tupI){
+    if(tuple[0] === k){
+      index = tupI;
+    }
+  });
+  if(index !== null){
+    bucketList.splice(index, 1);
+    this._storage.set(i, bucketList);
+    this._count--;
+    this.resizeCheck();
+  }
 };
+
+HashTable.prototype.resizeCheck = function(){
+  if(this._count/this._limit >= 0.75){
+    this.resize(this._limit*2);
+  } else if(this._count/this._limit <= 0.25){
+    this.resize(this._limit/2);
+  }
+};
+
+HashTable.prototype.resize = function(newLimit){
+  var temp = this._storage;
+  var thiz = this;
+  this._storage = LimitedArray(newLimit);
+  this._limit = newLimit;
+
+  temp.each(function(bucketList){
+    _.each(bucketList, function(tuple){
+      thiz.insert(tuple[0], tuple[1]);
+    });
+  });
+
+};
+
+
+
+
 
 
 
